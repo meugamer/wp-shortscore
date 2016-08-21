bdbd1c6c100e6370dffd2b7490584d5aadcbd79d
Marc Tönsing <marc.toensing@gmail.com>
2016-08-21 10:03:52 +0200

    Added basic error handling

--- wp-shortscore.php
+++ wp-shortscore.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Plugin Name: WP SHORTSCORE
 Description: Displays your SHORTSCORE at the bottom of the post. Add a custom field `shortscore_id` with
@@ -28,7 +29,8 @@ class WP_SHORTSCORE
         if (is_admin()) {
 
             add_action('save_post', array($this, 'getShortscore'));
-            add_action('add_meta_boxes', array($this,'shortscore_custom_meta'));
+            add_action('add_meta_boxes', array($this, 'shortscore_custom_meta'));
+            add_action('admin_notices', array($this, 'wp_shortscore_message'));
         }
     }
 
@@ -46,19 +48,21 @@ class WP_SHORTSCORE
 
     public function getShortscore($post_id)
     {
+
+
         // Checks save status
-        $is_autosave = wp_is_post_autosave( $post_id );
-        $is_revision = wp_is_post_revision( $post_id );
-        $is_valid_nonce = ( isset( $_POST[ 'shortscore_nonce' ] ) && wp_verify_nonce( $_POST[ 'shortscore_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+        $is_autosave = wp_is_post_autosave($post_id);
+        $is_revision = wp_is_post_revision($post_id);
+        $is_valid_nonce = (isset($_POST['shortscore_nonce']) && wp_verify_nonce($_POST['shortscore_nonce'], basename(__FILE__))) ? 'true' : 'false';
 
         // Exits script depending on save status
-        if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+        if ($is_autosave || $is_revision || !$is_valid_nonce) {
             return;
         }
 
         // Checks for input and sanitizes/saves if needed
-        if( isset( $_POST[ '_shortscore_id' ] ) ) {
-            update_post_meta( $post_id, '_shortscore_id', sanitize_text_field( $_POST[ '_shortscore_id' ] ) );
+        if (isset($_POST['_shortscore_id'])) {
+            update_post_meta($post_id, '_shortscore_id', sanitize_text_field($_POST['_shortscore_id']));
         }
 
         if (function_exists('get_post_meta') && get_post_meta($post_id, '_shortscore_id', true) != '') {
@@ -68,44 +72,59 @@ class WP_SHORTSCORE
             $json = file_get_contents($this->shortscore_baseurl . $this->shortscore_endpoint . $shortscore_id);
             $result = json_decode($json);
 
+            $error_code = false;
 
             if (!isset($result->game->id)) {
-                return;
+                $error_code = 'no-id';
             }
 
             if (!isset($result->game->url)) {
-                return;
+                $error_code = 'url';
             }
 
             if (!isset($result->shortscore->userscore)) {
-                return;
-            }
-
-            if (!isset($result->shortscore->userscore)) {
-                return;
+                $error_code = 'userscore';
             }
 
             if (!isset($result->shortscore->author)) {
-                return;
+                $error_code = 'author';
             }
 
             if (!isset($result->shortscore->summary)) {
-                return;
+                $error_code = 'summary';
             }
 
             if (!isset($result->game->title)) {
-                return;
+                $error_code = 'title';
             }
 
             if (!isset($result->shortscore->date)) {
-                return;
+                $error_code = 'date';
             }
 
             if (!isset($result->game->count)) {
+                $error_code = 'count';
+            }
+
+            if (!isset($result->shortscore->id)) {
+                $error_code = 'no-shortscore';
+            }
+
+            if($error_code){
+                add_filter('redirect_post_location', function ($location) use ($error_code) {
+                    return add_query_arg('wp-shortscore-error', $error_code, $location);
+                });
                 return;
             }
 
             $this->savePostMeta($post_id, '_shortscore_result', $result);
+
+            $msg_code = 'success';
+
+            add_filter('redirect_post_location', function ($location) use ($msg_code) {
+                return add_query_arg('wp-shortscore-msg', $msg_code, $location);
+            });
+
 
         }
 
@@ -119,17 +138,18 @@ class WP_SHORTSCORE
         $post_id = get_the_ID();
 
         if (
-            function_exists('get_post_meta') && get_post_meta($post_id, '_shortscore_result', true) != '' ) {
+            function_exists('get_post_meta') && get_post_meta($post_id, '_shortscore_result', true) != ''
+        ) {
 
             $result = get_post_meta($post_id, '_shortscore_result', true);
 
-            $shortscore_url =       $result->game->url;
-            $shortscore =     round($result->shortscore->userscore);
-            $shortscore_summary =   $result->shortscore->summary;
-            $shortscore_author =    $result->shortscore->author;
-            $shortscore_title =     $result->game->title;
-            $shortscore_date =      $result->shortscore->date;
-            $shortscore_count =     $result->game->count;
+            $shortscore_url = $result->game->url;
+            $shortscore = round($result->shortscore->userscore);
+            $shortscore_summary = $result->shortscore->summary;
+            $shortscore_author = $result->shortscore->author;
+            $shortscore_title = $result->game->title;
+            $shortscore_date = $result->shortscore->date;
+            $shortscore_count = $result->game->count;
 
             $shortscore_html = '<div class="type-game">';
             $shortscore_html .= '<div class="hreview shortscore-hreview">';
@@ -140,9 +160,9 @@ class WP_SHORTSCORE
             $shortscore_html .= '<span class="item"> <a class="score" href="' . $shortscore_url . '"><strong class="fn">' . $shortscore_title . '</strong></a>: </span>';
             $shortscore_html .= '<span class="summary">' . $shortscore_summary . '</span><span class="reviewer vcard"> – <span class="fn">' . $shortscore_author . '</span></span>';
             $shortscore_html .= '</div>';
-            $shortscore_html .= '<div class="link"><a href="' . $shortscore_url . '">' . sprintf(__('%s', 'wp-shortscore'),'<span class="votes">' . sprintf(_n('one user review', '%s user reviews', $shortscore_count, 'wp-shortscore'),$shortscore_count ) . '</span> ') . __('on','wp-shortscore') . ' SHORTSCORE.org</a> ' . __('to','wp-shortscore') . ' '. $shortscore_title . '</div>';
+            $shortscore_html .= '<div class="link"><a href="' . $shortscore_url . '">' . sprintf(__('%s', 'wp-shortscore'), '<span class="votes">' . sprintf(_n('one user review', '%s user reviews', $shortscore_count, 'wp-shortscore'), $shortscore_count) . '</span> ') . __('on', 'wp-shortscore') . ' SHORTSCORE.org</a> ' . __('to', 'wp-shortscore') . ' ' . $shortscore_title . '</div>';
             $shortscore_html .= '<span class="dtreviewed">' . $shortscore_date . '</span> ';
-            $shortscore_html .= '<span class="outof">' . sprintf(__('out of %s.','wp-shortscore'),'<span class="best">10</span>').'</span>';
+            $shortscore_html .= '<span class="outof">' . sprintf(__('out of %s.', 'wp-shortscore'), '<span class="best">10</span>') . '</span>';
             $shortscore_html .= '</div>';
             $shortscore_html .= '</div>';
 
@@ -177,26 +197,73 @@ class WP_SHORTSCORE
     /**
      * Adds a meta box to the post editing screen
      */
-    public function shortscore_custom_meta() {
-        add_meta_box( 'shortscore_meta', __( __('Add SHORTSCORE','wp-shortscore'), 'prfx-textdomain' ), array($this,'shortscore_meta_callback'), 'post' );
+    public function shortscore_custom_meta()
+    {
+        add_meta_box('shortscore_meta', __(__('Add SHORTSCORE', 'wp-shortscore'), 'prfx-textdomain'), array($this, 'shortscore_meta_callback'), 'post');
     }
 
     /**
      * Outputs the content of the meta box
      */
-    public function shortscore_meta_callback( $post ) {
+    public function shortscore_meta_callback($post)
+    {
 
-        wp_nonce_field( basename( __FILE__ ), 'shortscore_nonce' );
-        $shortscore_stored_meta = get_post_meta( $post->ID );
+        wp_nonce_field(basename(__FILE__), 'shortscore_nonce');
+        $shortscore_stored_meta = get_post_meta($post->ID);
         ?>
         <p>
-            <label for="_shortscore_id" class="prfx-row-title"><?php _e( 'Please input SHORTSCORE ID', 'wp-shortscore' )?></label>
-            <input type="text" name="_shortscore_id" id="_shortscore_id" value="<?php if ( isset ( $shortscore_stored_meta['_shortscore_id'] ) ) echo $shortscore_stored_meta['_shortscore_id'][0]; ?>" />
+            <label for="_shortscore_id"
+                   class="prfx-row-title"><?php _e('Please input SHORTSCORE ID', 'wp-shortscore') ?></label>
+            <input type="text" name="_shortscore_id" id="_shortscore_id"
+                   value="<?php if (isset ($shortscore_stored_meta['_shortscore_id'])) echo $shortscore_stored_meta['_shortscore_id'][0]; ?>"/>
         </p>
-        <p><?php _e( 'You can find the SHORTSCORE ID next to your submitted SHORTSCORE on', 'wp-shortscore' )?> <a href="http://shortscore.org">SHORTSCORE.org</a></p>
+        <p><?php _e('You can find the SHORTSCORE ID next to your submitted SHORTSCORE on', 'wp-shortscore') ?> <a
+                href="http://shortscore.org">SHORTSCORE.org</a></p>
         <?php
     }
 
+
+    /**
+     * Admin notices
+     **/
+    public function wp_shortscore_message()
+    {
+        if (array_key_exists('wp-shortscore-error', $_GET)) { ?>
+            <div class="error">
+            <p>
+                <?php
+                switch ($_GET['wp-shortscore-error']) {
+                    case 'no-shortscore':
+                        echo 'This SHORTSCORE ID does not exist';
+                        break;
+                    default:
+                        echo 'An error ocurred when saving the SHORTSCORE.';
+                        break;
+                }
+                ?>
+            </p>
+            </div><?php
+
+        }
+
+        if (array_key_exists('wp-shortscore-msg', $_GET)) { ?>
+            <div class="notice notice-success">
+            <p>
+                <?php
+                switch ($_GET['wp-shortscore-msg']) {
+                    case 'success':
+                        _e('Valid SHORTSCORE ID found and saved to post successfully','wp-shortscore');
+                        break;
+                    default:
+                        _e('SHORTSCORE ID saved','wp-shortscore');
+                        break;
+                }
+                ?>
+            </p>
+            </div><?php
+
+        }
+    }
 }
 
 new WP_SHORTSCORE();
